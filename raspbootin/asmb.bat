dfa4159c1489a5061f705afe0678af167ba4d108
minja <minja@maja2.m>
2019-09-25 13:16:45 +0200

    Introduced symbols.

--- raspbootin/asmb.bat
+++ raspbootin/asmb.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM Usage: asm.bat program.asm
 if %1asdf==asdf goto usage
-customasm.exe -i instruction-set.txt -o %1.bin -f binary %1
+customasm.exe -i instruction-set.txt -o %1.bin -f binary %1 -s %1.sym
 goto end
 :usage
 ECHO Usage: asm.bat program.asm
--- raspbootin/hello_world2.asm
+++ raspbootin/hello_world2.asm
@@ -8,14 +8,14 @@
 	mov.w r2, VIDEO		     ; r2 points to the beginning of the video memory
 	mov.w r1, hello  			 ; r1 holds the address of the "HELLO WORLD" string
 again:	
-	ld.s r0, [r1]          ; load r0 with the content of the memory location to which r1 points (current character)
-	cmp.s r0, 0            ; if the current character is 0 (string terminator),
+	ld.b r0, [r1]          ; load r0 with the content of the memory location to which r1 points (current character)
+	cmp.b r0, 0            ; if the current character is 0 (string terminator),
 	jz end                 ; go out of this loop 
 	st.s [r2], r0  	 			 ; store the character at the VIDEO_0 + r2 
-	add.s r1, 2            ; move to the next character
+	add.s r1, 1            ; move to the next character
 	add.s r2, 2            ; move to the next location in the video memory
 	j again                ; continue with the loop
 end:	
 	halt
 hello:
-	#str16 "HELLO WORLD!ASDF ASDF ASDF\0"
+	#str "HELLO WORLD!ASDF ASDF ASDF\0"
